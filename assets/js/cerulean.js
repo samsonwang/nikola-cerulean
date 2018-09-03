7f0614c0cc9b74d15c7cbc722d10e38f9ea180e3
SamsonWang <wangzhilv@gmail.com>
2018-09-03 23:35:50 +0800

    add scroll top button

--- assets/js/cerulean.js
+++ assets/js/cerulean.js
@@ -1,23 +1,19 @@
 
+function initScrollTop () {
+  $("#scroll-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
 
-$("a[href='#top']").click(function() {
-  $("html, body").animate({ scrollTop: 0 }, "slow");
-  return false;
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 20) {
+      $("#scroll-top").css("display", "block");
+    } else {
+      $("#scroll-top").css("display", "none");
+    }
+  });
+}
+
+$(document).ready(function () {
+  initScrollTop();
 });
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
